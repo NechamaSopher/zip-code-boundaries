024fdd4af9ca134fe0c9552e440508d90cc5124e
nechama Ini <nechami2212@gmail.com>
2024-09-04 09:11:43 +0300

    Auto-generated commit

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  MAPBOX_API_KEY: 'pk.eyJ1IjoibmVjaGFtYWluaSIsImEiOiJjbTBreWRjZXIwbHM0MmpwamZha3E1ODk5In0.R537FC9MpnhVNu-hzMOYng',
-  BOUNDARIES_IO_API_KEY: 'de49b1a0f3msh5749e930fb537d1p16bc91jsn43759a94a7ed'
-};
